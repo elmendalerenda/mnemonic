c2bd9c5e81048ede29d9e2c9936e51f42a0f2efe
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2016-03-12 17:25:09 +0000

    simple integration with server

---
+++ client/app/scripts/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+  'use strict';
+
+  function loadImage() {
+    $.get('/images?q=' + this.value, function(data){
+      var imageUrl = data.images[0];
+      $('#main-image').attr('src', imageUrl);
+    });
+  }
+
+  $('#numbers-input').on('blur', loadImage);
+})(jQuery);
